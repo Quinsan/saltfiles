b7ad81a9e057b9910bd99d617afeef63bd4465dd
Quinsan <jussipelk@gmail.com>
2020-05-17 13:46:01 +0300

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -9,7 +9,7 @@ read MASTERIP
 echo "Anna minionin nimi:"
 read MINIONID
 
-echo "master: $MASTERIP\nid: $MINIONID" | sudo tee -a /etc/salt/minion
+echo -e "master: $MASTERIP\nid: $MINIONID" | sudo tee -a /etc/salt/minion
 
 echo "Restarting salt-minion"
 sudo systemctl restart salt-minion.service
